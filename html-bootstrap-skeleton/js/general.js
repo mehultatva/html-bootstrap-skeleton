01e154c7876e891ec6db812b0aa87b1f9db3cccd
mehultatva <43134881+mehultatva@users.noreply.github.com>
2020-01-03 17:51:34 +0530

    Initial commit

---
+++ html-bootstrap-skeleton/js/general.js
@@ -0,0 +1,112 @@
+// browser specific js
+var nVer = navigator.appVersion;
+var nAgt = navigator.userAgent;
+var browserName = navigator.appName;
+var fullVersion = "" + parseFloat(navigator.appVersion);
+var majorVersion = parseInt(navigator.appVersion, 10);
+var nameOffset, verOffset, ix;
+
+
+// In Opera, the true version is after "Opera" or after "Version"
+if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+    browserName = "Opera";
+    fullVersion = nAgt.substring(verOffset + 6);
+    if ((verOffset = nAgt.indexOf("Version")) != -1) fullVersion = nAgt.substring(verOffset + 8);
+}
+// In MSIE, the true version is after "MSIE" in userAgent
+else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+    browserName = "Microsoft Internet Explorer";
+    fullVersion = nAgt.substring(verOffset + 5);
+}
+// In Chrome, the true version is after "Chrome"
+else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+    browserName = "Chrome";
+    fullVersion = nAgt.substring(verOffset + 7);
+}
+// In Safari, the true version is after "Safari" or after "Version"
+else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+    browserName = "Safari";
+    fullVersion = nAgt.substring(verOffset + 7);
+    if ((verOffset = nAgt.indexOf("Version")) != -1) fullVersion = nAgt.substring(verOffset + 8);
+}
+// In Firefox, the true version is after "Firefox"
+else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+    browserName = "Firefox";
+    fullVersion = nAgt.substring(verOffset + 8);
+}
+// In most other browsers, "name/version" is at the end of userAgent
+else if ((nameOffset = nAgt.lastIndexOf(" ") + 1) < (verOffset = nAgt.lastIndexOf("/"))) {
+    browserName = nAgt.substring(nameOffset, verOffset);
+    fullVersion = nAgt.substring(verOffset + 1);
+    if (browserName.toLowerCase() == browserName.toUpperCase()) {
+        browserName = navigator.appName;
+    }
+}
+var OSName = "UnknownOS";
+if (navigator.appVersion.indexOf("Win") != -1) OSName = "Windows";
+if (navigator.appVersion.indexOf("Mac") != -1) OSName = "MacOS";
+if (navigator.appVersion.indexOf("X11") != -1) OSName = "UNIX";
+if (navigator.appVersion.indexOf("Linux") != -1) OSName = "Linux";
+$("body,html")
+.addClass(browserName.toLowerCase())
+.addClass(OSName.toLowerCase());
+
+if (navigator.userAgent.match(/iP(hone|od|ad)/i)) {
+    $("body,html").addClass("browser-ios");
+}
+
+function isIE() {
+    ua = navigator.userAgent;
+    /* MSIE used to detect old browsers and Trident used to newer ones*/
+    var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+
+    return is_ie;
+}
+/* Equal Height */
+function equalHeight() {
+	$.fn.extend({
+		equalHeights: function() {
+			var top = 0;
+			var row = [];
+			var classname = ('equalHeights' + Math.random()).replace('.', '');
+			$(this).each(function() {
+				var thistop = $(this).offset().top;
+				if (thistop > top) {
+					$('.' + classname).removeClass(classname);
+					top = thistop;
+				}
+				$(this).addClass(classname);
+				$(this).height('auto');
+				var h = (Math.max.apply(null, $('.' + classname).map(function() {
+					return $(this).outerHeight();
+				}).get()));
+				$('.' + classname).height(h);
+			}).removeClass(classname);
+		}
+	});
+	$('.classname').equalHeights();
+}
+
+/* Document Ready */
+$(document).ready(function() {
+	equalHeight();
+
+	$("a[href='#']").click(function(e) {
+		e.preventDefault();
+	});
+
+
+
+})
+
+
+/* Window Resize */
+$(window).resize(function() {
+	equalHeight();
+});
+
+
+/* Window Load */
+$(window).on("load",function() {
+	equalHeight();
+});
